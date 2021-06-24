0d058c97daff17a4b78c148f98c4709beae50c50
Shivam Sharma <shivam691999@gmail.com>
2021-06-24 23:24:43 +0530

    first commit

---
+++ src/screens/testingPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class testingPage extends Component {
+    render() {
+        return (
+            <div>
+                For Testing Purposes
+            </div>
+        )
+    }
+}
