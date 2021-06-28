7895fcf6393e31c98ae9ef81a6c1c2ed0e32db8c
Rohit Yadav <rohit12242@Rohits-MacBook-Air.local>
2021-06-28 17:08:21 +0530

    side bar done

---
+++ src/screens/sidebar/account/account.js
@@ -0,0 +1,46 @@
+import React,{Component} from 'react';
+import accountLogo from '../../Assets/accountlogo.png';
+import styled, { ThemeProvider } from "styled-components";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+
+
+const AvatarContainer = styled.div`
+  display: flex;
+  margin-top:25px;
+  margin-bottom: 14px;
+  & > * {
+    margin: 4px;
+  }
+`;
+
+const AvatarLabel = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 62px;
+`;
+
+
+export default class Account extends Component{
+    render() {
+        return(
+            /*<div>
+                <img src={accountLogo} alt="accountLogo"/>
+                <span>
+                    <button>My Addr</button>
+                    <p style={{color:'red'}}>$12500</p>
+                </span>
+            </div>*/
+            <AvatarContainer>
+                <AvatarLabel>
+                    <Avatar
+                        style={{ marginRight: "14px" }}
+                        alt="Jack Sparrow"
+                        src={accountLogo}
+                    />
+                    <Typography variant="body2" color="primary"> Jack Sparrow</Typography>
+                </AvatarLabel>
+            </AvatarContainer> 
+        );
+    }
+}
