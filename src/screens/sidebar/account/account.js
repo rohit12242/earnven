764408429370e7dad663753a02ee1720af00273c
Rohit Yadav <rohit12242@Rohits-MacBook-Air.local>
2021-06-28 18:41:56 +0530

    resolved warnings

--- src/screens/sidebar/account/account.js
+++ src/screens/sidebar/account/account.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 import accountLogo from '../../Assets/accountlogo.png';
-import styled, { ThemeProvider } from "styled-components";
+import styled from "styled-components";
 import Typography from "@material-ui/core/Typography";
 import Avatar from "@material-ui/core/Avatar";
 
