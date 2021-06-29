cf23152d804c822fd4c1dbde5ae88a5b51b1d753
Rohit Yadav <rohit12242@Rohits-MacBook-Air.local>
2021-06-30 01:45:33 +0530

    dashboard integration intermediate

--- src/screens/dashboard/dashboard.js
+++ src/screens/dashboard/dashboard.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import './dashboard.css';
 import PortfolioPerf from './portfolioperf/portfolioperf';
+import AllAssets from '../../components/allAssetsMini';
+import TotalValueBox from '../../components/totalValueBox';
 
 export default class Dashboard extends Component {
     render() {
         return (
             <div className='dashboard'>
-                <div className='portfolio' style={{border:'1px',borderStyle:'solid',borderColor:'#737373',height:'360px',borderRadius:'7px'}}>
-                    <p style={{color:'white', marginTop:'0',height:'0',paddingTop:'10px',paddingLeft:'10px'}}>Portfolio Performance</p>
-                    <PortfolioPerf />
+                <div>
+                    <div className='portfolio' style={{border:'1px',borderStyle:'solid',borderColor:'#737373',height:'360px',borderRadius:'7px'}}>
+                        <p style={{color:'white', marginTop:'0',height:'0',paddingTop:'10px',paddingLeft:'10px'}}>Portfolio Performance</p>
+                        <PortfolioPerf />
+                    </div>
+                    <div className='total-value'>
+                            <TotalValueBox />
+                        </div>
                 </div>
+                <div className='assets'>
+                    <AllAssets />
+                </div>
+                
             </div>
         )
     }
--- src/screens/header/header.js
+++ src/screens/header/header.js
@@ -1,11 +1,32 @@
 import React,{Component} from 'react';
 import SearchInput from '../../components/searchInput';
+import NetworkDropDown from '../../components/networkDropDown';
+import MenuListComposition from '../../components/gasDropDownMenu';
+import LanguageDropDown from '../../components/languageDropDown';
+import HelpDropDown from '../../components/helpDropDown';
+import './header.css'
 
 export default class Header extends Component{
     render(){
         return(
             <div className='header'>
-                <SearchInput />
+                <div className='search-bar'>
+                     <SearchInput />
+                </div>
+                <div className='network-dropdown'>
+                    <NetworkDropDown />
+                </div>
+                <div className='gas-dropdown'>
+                    <MenuListComposition/>
+                </div>
+                <div className='language-dropdown'>
+                    <LanguageDropDown />
+                </div>
+                <div className='help-dropdown'>
+                    <HelpDropDown />
+                </div>
+                {/* <hr style={{borderTop:'1px solid #737373'}}></hr> */}
+            
             </div>
         )
     }
