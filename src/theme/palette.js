588ad9186b0ea81ba05f249f518c8a7f4a5be669
Rohit Yadav <rohit12242@Rohits-MacBook-Air.local>
2021-07-06 01:51:50 +0530

    color change push

--- src/theme/palette.js
+++ src/theme/palette.js
@@ -107,7 +107,7 @@ const palette = {
   gradients: GRADIENTS,
   divider: GREY[500_24],
   text: { primary: GREY[800], secondary: GREY[600], disabled: GREY[500] },
-  background: { paper: '#fff', default: '#000', neutral: GREY[200] },
+  background: { paper: '#fff', default: '#0E1214', neutral: GREY[200] },
   action: {
     active: GREY[600],
     hover: GREY[500_8],
