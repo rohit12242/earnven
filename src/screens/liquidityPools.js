500ef6e9058ddeb875858d9c2e4a27c818fb6713
Shivam Sharma <shivam691999@gmail.com>
2021-07-26 17:44:52 +0530

    liquidity Pool status

--- src/screens/liquidityPools.js
+++ src/screens/liquidityPools.js
@@ -1,27 +1,101 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios'
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import TransparentButton from '../components/TransparentButton'
+import AmountInput from '../components/amountInput'
 
 export default function LiquidityPools() {
 
     const [Data, setData] = useState([])
     const [Content, setContent] = useState('')
 
+    // async function getImageURL(tokenID){
+    //     console.log(tokenID)
+    //     var resp;
+    //     await axios.get(`https://api.ethplorer.io/getTokenInfo/${tokenID}?apiKey=EK-qSPda-W9rX7yJ-UY93y`,{},{})
+    //     .then(async(response) => {
+    //         if(response.data.image){
+    //             // console.log(response.data.image)
+    //             resp = response.data.image
+    //         }
+    //     })
+    //     return resp
+    // }
+
+    // getImageURL('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
+
     useEffect(() => {
         var content = Data.map((object)=>
-        <div style={{height:'70px',
-                    width:'80%',
-                    border:'1px solid white',
-                    margin:'auto',
-                    borderRadius:'5px'
-        }}>
+        <>  
+
+        <br/>
+        <Accordion style={{background:'transparent', marginLeft:'40px', color:'white', width:'90%', border:'1px', borderColor:'white', borderStyle:'solid'}}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+        <div style={{height:'60px', width:'100%'}}>
+            <div style={{display:'inline-block', width:'5%'}}>
+                
+            </div>
+
+            <div style={{display:'inline-block', width:'25%'}}>
+                <br/>
+                {object.token0.symbol} {object.token1.symbol}
+            </div> 
+
+            <div style={{display:'inline-block', width:'30%'}}>
+                <br/>
+                {parseFloat(object.reserveUSD).toFixed(2)} USD
+            </div>
+
+            <div style={{display:'inline-block', width:'30%', marginTop:'10px'}}>
+                
+                {((parseInt(object.dailyVolumeUSD)*0.003)/parseInt(object.reserveUSD)*100*365).toFixed(2)} % (Yearly)<br/>
+                {((parseInt(object.dailyVolumeUSD)*0.003)/parseInt(object.reserveUSD)*100*7).toFixed(2)} % (Weekly)
+            </div>
+            <div style={{display:'inline-block', width:'10%',}}>
             
+                <img style={{ height:'30px', width:'30px',display:'inline-block'}} alt='' src={`https://ethplorer.io${object.token0.image}`}/>
+                &nbsp;&nbsp;&nbsp;
+                <img style={{ height:'30px', width:'30px',display:'inline-block'}} alt='' src={`https://ethplorer.io${object.token1.image}`}/>
+            
+            </div>
+
         </div>
+        </AccordionSummary>
+        <AccordionDetails>
+        {/* <AmountInput value={object.newAllowance} onChange={(e)=>{object.newAllowance=e.target.value}}/>&nbsp;${object.symbol}<br/>
+        <font style={{fontSize:'10px'}}> &nbsp;&nbsp;**Set Approval <b>0</b> to revoke Approval.</font>
+        <br/>
+        <TransparentButton value='Set new Allowance' onClick={async(e)=>{
+            let web3 = window.web3;
+            const accounts = await web3.eth.getAccounts()
+            let Token = await new web3.eth.Contract(ERC20ABI, tokenAddress)
+            await Token.methods.approve(object.address, (object.newAllowance*(10**18)).toString()).send({ from : accounts[0]})
+        }}/> */}
+        </AccordionDetails>
+      </Accordion>
+        
+        </>
         )
         setContent(content)
     }, [Data])
 
     useEffect(() => {
         // console.log('lol')
+        var d = new Date()
+        var day = d.getUTCDate()
+        var month = d.getUTCMonth()
+        var year = d.getUTCFullYear()
+        var offset = new Date(year, month, day).getTimezoneOffset() * 60
+        var epoch = new Date(year, month, day).getTime()/1000 - offset;
+        
+        // console.log(epoch)
         async function getData(){
             await axios.post(`https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2`,
             {
@@ -29,7 +103,7 @@ export default function LiquidityPools() {
                 {
                     pairDayDatas(first: 20,
                        where:{
-                        date:1626998400
+                        date:${epoch}
                       }
                         orderBy:dailyVolumeUSD,
                          orderDirection: desc)
@@ -51,9 +125,27 @@ export default function LiquidityPools() {
                     }
                 }`})
                 .then(async(response) => {
-                    // console.log(response.data.data.pairDayDatas)
                     if(response.data.data){
-                        setData(response.data.data.pairDayDatas)
+                        var res = response.data.data.pairDayDatas
+                        for(var i =0; i<res.length; i++){
+                            await axios.get(`https://api.ethplorer.io/getTokenInfo/${res[i].token0.id}?apiKey=EK-qSPda-W9rX7yJ-UY93y`,{},{})
+                            .then(async(response) => {
+                                if(response.data.image){
+                                    // console.log(response.data.image)
+                                    res[i].token0.image = response.data.image
+                                }
+                            })
+                            await axios.get(`https://api.ethplorer.io/getTokenInfo/${res[i].token1.id}?apiKey=EK-qSPda-W9rX7yJ-UY93y`,{},{})
+                            .then(async(response) => {
+                                if(response.data.image){
+                                    res[i].token1.image = response.data.image
+
+                                }
+                            })
+                        }
+                        setData(res)
+                    console.log(res)
+
                     }
             })
         }   
@@ -62,7 +154,10 @@ export default function LiquidityPools() {
 
     return (
         <div>
-            <br/><br/>
+            <center>
+            <h1 style={{fontSize:'40px', color:'white'}}>Liquidity Pools</h1>
+            </center>
+            
             {Content}
         </div>
     )
